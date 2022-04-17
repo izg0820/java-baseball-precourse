0837ace3d101d0c7514596c9c6b170eea80559b3
izg0820 <izg0820@naver.com>
2022-04-17 23:25:44 +0900

    refactor: change the way set up for multiple test cases

--- src/test/java/service/AnswerCheckServiceTest.java
+++ src/test/java/service/AnswerCheckServiceTest.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
@@ -15,14 +18,18 @@ import static org.assertj.core.api.Assertions.*;
 
 class AnswerCheckServiceTest {
 
-    @Test
-    void 채점() {
-        OutputStream out = new ByteArrayOutputStream();
+
+    OutputStream out = null;
+    @BeforeEach
+    void setUp() {
+        out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
-        AnswerCheckService.answerCheck("475", "345");
-        AnswerCheckService.answerCheck("789", "345");
-        AnswerCheckService.answerCheck("345", "345");
-        assertThat(out.toString().trim()).contains("1볼 1스트라이크", "낫싱", "3스트라이크");
+    }
+    @ParameterizedTest
+    @CsvSource(value = {"475:1볼 1스트라이크", "789:낫싱", "345:3스트라이크"}, delimiter = ':')
+    void 채점(String myAnswer, String result) {
+        AnswerCheckService.answerCheck(myAnswer, "345");
+        assertThat(out.toString().trim()).isEqualTo(result);
     }
 
 }
