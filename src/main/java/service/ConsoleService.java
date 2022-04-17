dba44ceb7637c9b0533eee88cdf27439daea0d69
izg0820 <izg0820@naver.com>
2022-04-17 17:18:21 +0900

    refactor: fix typo and add readLine and printfConsole method
    Fix the typo of message.
    Add readLine method to read value in console
    and printfConsole method to print a variable.
    Change printConsole method to contain \n character.

--- src/main/java/service/ConsoleService.java
+++ src/main/java/service/ConsoleService.java
@@ -2,11 +2,13 @@ package service;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashMap;
+
 public class ConsoleService {
 
     public enum Message {
         ENTER_NUMBER ("숫자를 입력해주세요 : "),
-        GAME_OVER("3개의숫자를모두맞히셨습니다! 게임종료"),
+        GAME_OVER("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
         GAME_RESTART("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
         NOTHING("낫싱");
 
@@ -20,27 +22,37 @@ public class ConsoleService {
             return value;
         }
     }
+
+    public static String readLine() {
+        return Console.readLine();
+    }
     private static void printConsole(String message) {
+        System.out.println(message);
+    }
+
+    private static void printfConsole(String message) {
         System.out.printf("%s", message);
     }
 
-    public static void enterNumber() {
-        printConsole(Message.ENTER_NUMBER.getValue());
+    public static String enterNumber() {
+        printfConsole(Message.ENTER_NUMBER.getValue());
+        return readLine();
     }
 
-    public static void gameOVer() {
+    public static void gameOver() {
         printConsole(Message.GAME_OVER.getValue());
     }
 
-    public static void gameRestart() {
+    public static String gameRestart() {
         printConsole(Message.GAME_RESTART.getValue());
+        return Console.readLine();
     }
 
     public static void nothing() {
         printConsole(Message.NOTHING.getValue());
     }
 
-    public static void result(int ballCount, int strikeCount) {
+    public static void result(int ballCount, int strikeCount ) {
         printConsole(setResultScript(ballCount, strikeCount));
     }
 
@@ -48,7 +60,7 @@ public class ConsoleService {
         return (setBallCount(ballCount) + " " + setStrikeCount(strikeCount)).trim();
     }
 
-    public static String setBallCount(int ballCount) {
+    private static String setBallCount(int ballCount) {
         String ball = "";
         if(ballCount > 0) {
             ball = ballCount + "볼";
@@ -56,7 +68,7 @@ public class ConsoleService {
         return ball;
     }
 
-    public static String setStrikeCount(int strikeCount) {
+    private static String setStrikeCount(int strikeCount) {
         String strike = "";
         if(strikeCount > 0) {
             strike = strikeCount + "스트라이크";
